1e4506828627302b67ccd893cf9c965bd2e9cb64
Tatiana Vasileva <tatyanushka21@mail.ru>
2022-09-18 22:59:55 +0300

    'negativ numper input fanction added'

--- src/Main.java
+++ src/Main.java
@@ -3,73 +3,72 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] products =// РјР°СЃСЃРёРІ С‚РёРїР° СЃС‚СЂРёРЅРі РЅР°Р·РІР°РЅРёСЏ
-                {"РњРѕР»РѕРєРѕ",
-                        "РЎС‹СЂ",
-                        "РҐР»РµР±",
-                        "Р™РѕРіСѓСЂС‚",
-                        "РЎРІРµРєР»Р°"
+        String[] products =// массив типа стринг названия
+                {"Молоко",
+                        "Сыр",
+                        "Хлеб",
+                        "Йогурт",
+                        "Свекла"
                 };
-        int[] price = {70, 400, 40, 88, 20};//РёРЅС‚РѕРІС‹Р№ РјР°СЃСЃРёРІ С†РµРЅР°
-        int[] numer = new int[5];//СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР°
-        System.out.println("РЎРїРёСЃРѕРє РїСЂРѕРґСѓРєС‚РѕРІ Рё СЃС‚РѕРёРјРѕСЃС‚Рё РµРґРёРЅРёС†С‹:");
-        //System.out.println(new Product[]{new Product(70, "РњРѕР»РѕРєРѕ"), new Product(400, "РЎС‹СЂ"),
-        //new Product(40, "РҐР»РµР±"), new Product(88, "Р™РѕРіСѓСЂС‚"), new Product(20, "РЎРІРµРєР»Р°")
-// С‡С‚РѕР±С‹ РёР·Р±РµР¶Р°С‚СЊ Р±РµСЃРєРѕРЅРµС‡РЅРѕРіРѕ РєРѕРїРёСЂРѕРІР°РЅРёСЏ РїСЂРѕР±СѓРµРј С‡РµСЂРµР· С†РёРєР» for
-        for (int i = 0; i < products.length; i++) { // length С‡С‚РѕР±С‹ СѓР·РЅР°С‚СЊ РєР°РєРѕР№ СЂР°Р·РјРµСЂ
-            System.out.println((i + 1) + "." + products[i] + " " + price[i] + " СЂСѓР±.РµРґ");
+        int[] price = {70, 400, 40, 88, 20};//интовый массив цена
+        System.out.println("Список продуктов и стоимости единицы:");
+        //System.out.println(new Product[]{new Product(70, "Молоко"), new Product(400, "Сыр"),
+        //new Product(40, "Хлеб"), new Product(88, "Йогурт"), new Product(20, "Свекла")
+// чтобы избежать бесконечного копирования пробуем через цикл for
+        for (int i = 0; i < products.length; i++) { // length чтобы узнать какой размер
+            System.out.println((i + 1) + "." + products[i] + " " + price[i] + " руб.ед");
         }
         int total = 0;
-        int productNum = 0;
+        int productNum;
         int[] yourBasket = new int[products.length];
-        int amount = 0;
+        int amount;
 
         while (true) {
-            System.out.println(" Р’С‹Р±РµСЂРёС‚Рµ С‚РѕРІР°СЂ Рё РєРѕР»РёС‡РµСЃС‚РІРѕ РёР»Рё РІРІРµРґРёС‚Рµ end");
-            String input = scanner.nextLine();//РњРѕР»РѕРєРѕ СЃС‹СЂ С…Р»РµР± Р№РѕРіСѓСЂС‚ СЃРІРµРєР»Р°
+            System.out.println(" Выберите товар и количество или введите end");
+            String input = scanner.nextLine();//Молоко сыр хлеб йогурт свекла
             if (input.equals("end")) {
                 break;
             }
-            try { // Р±Р»РѕРє try, РіРґРµ РјРѕР¶РµС‚ РІРѕР·РЅРёРєРЅСѓС‚СЊ РѕС€РёР±РєР°
-                String[] part = input.split(" ");//СЂР°Р·РґРµР»РёС‚РµР»СЊ
+            try { // блок try, где может возникнуть ошибка
+                String[] part = input.split(" ");//разделитель
                 if (part.length != 2) {
 
-                    System.out.println("РќРµРєРѕСЂСЂРµРєС‚РЅС‹Р№ РІРІРѕРґ! РќСѓР¶РЅРѕ РІРІРµСЃС‚Рё РґРІР° С‡РёСЃР»Р°");
+                    System.out.println("Некорректный ввод! Нужно ввести два числа");
                     continue;
                 }
 
                 productNum = Integer.parseInt(part[0]) - 1;
                 if ((productNum + 1) > products.length || productNum + 1 <= 0) {
-                    System.out.println("РћС€РёР±РєР°.РќСѓР¶РЅРѕ РІС‹Р±СЂР°С‚СЊ РЅРѕРјРµСЂ РїСЂРѕРґСѓРєС‚Р° РёР· СЃРїРёСЃРєР°");
+                    System.out.println("Ошибка.Нужно выбрать номер продукта из списка");
                     continue;
                 }
                 amount = Integer.parseInt(part[1]);
-                if (amount <= 0) {
-                    System.out.println("РћС€РёР±РєР°.РљРѕР»РёС‡РµСЃС‚РІРѕ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РјРµРЅСЊС€Рµ 0");
-
+                if (yourBasket[productNum] + amount < 0) { //если введенное количество меньше, чем товара в корзине
+                    System.out.println("Количество товара в корзине не может быть меньше 0");
                     continue;
                 }
+                if (amount == 0) {//если введен 0
+                    total -= (yourBasket[productNum] * price[productNum]);//цена всей корзины минус стоимость товара в ней
+                    yourBasket[productNum] = 0;
+                } else {
+                    yourBasket[productNum] = yourBasket[productNum] + amount;
+                    int sum = amount * price[productNum];
+                    total += sum;
+                }
 
-            } catch (NumberFormatException e) { //СЏС‡РµР№РєР° РІ РєРѕС‚РѕСЂСѓСЋ РїРѕР»РѕР¶РёС‚СЊ РµСЃР»Рё РѕС€РёР±РєР°
-                System.out.println("РћС€РёР±РєР°. РќСѓР¶РЅРѕ РІРІРѕРґРёС‚СЊ С‚РѕР»СЊРєРѕ С‡РёСЃР»Р°");
+            } catch (NumberFormatException e) { //ячейка в которую положить если ошибка
+                System.out.println("Ошибка. Нужно вводить только числа");
                 continue;
             }
 
-            numer[productNum] += amount;//РїСЂРёСЃРІРѕРёС‚СЊ
-
-            int sum = amount * price[productNum];//РїРѕРґСЃС‡РёС‚Р°С‚СЊ
-
-            yourBasket[productNum] = yourBasket[productNum] + amount;//РєРѕСЂР·РёРЅР°
-            total += sum;//РІС‹РІРѕРґ РёС‚РѕРіР°
-
-            System.out.println("Р’Р°С€Р° РєРѕСЂР·РёРЅР°:");
-            for (int m = 0; m < products.length; m++) { //СЃРєР»РѕР¶РёС‚СЊ
+            System.out.println("Ваша корзина:");
+            for (int m = 0; m < products.length; m++) { //скложить
                 int currentPrice = yourBasket[m] * price[m];
                 if (yourBasket[m] > 0) {
-                    System.out.println(products[m] + " " + yourBasket[m] + " РµРґ. " + currentPrice + " СЂСѓР±. РІ СЃСѓРјРјРµ");
+                    System.out.println(products[m] + " " + yourBasket[m] + " ед. " + currentPrice + " руб. в сумме");
                 }
             }
-            System.out.println("РС‚РѕРіРѕ: " + total + " СЂСѓР±");//РёС‚РѕРі
+            System.out.println("Итого: " + total + " руб");//итог
+        }
         }
     }
-}
